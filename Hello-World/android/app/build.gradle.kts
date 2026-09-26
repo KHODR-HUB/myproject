62697103e7c767576afc004e449b1a1eb16e3980
KHODR-HUB <khodremail@gmail.com>
2026-09-25 23:24:49 -0400

    Update build.gradle.kts

--- Hello-World/android/app/build.gradle.kts
+++ Hello-World/android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+  }
     android {
     namespace = "com.example.helloworld"
     compileSdk = flutter.compileSdkVersion
@@ -16,11 +17,8 @@ plugins {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.helloworld"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+       applicationId = "com.example.helloworld"
+         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -28,8 +26,6 @@ plugins {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
