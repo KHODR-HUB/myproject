46f5dff1fffb481d339efe0dcacc0092dba1ad37
KHODR-HUB <khodremail@gmail.com>
2026-09-25 23:59:44 -0400

    Add files via upload

--- Hello-World/android/app/build.gradle.kts
+++ Hello-World/android/app/build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-  }
-    android {
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
     namespace = "com.example.helloworld"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
@@ -17,8 +20,11 @@ plugins {
     }
 
     defaultConfig {
-       applicationId = "com.example.helloworld"
-         minSdk = flutter.minSdkVersion
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.helloworld"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -26,6 +32,8 @@ plugins {
 
     buildTypes {
         release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
